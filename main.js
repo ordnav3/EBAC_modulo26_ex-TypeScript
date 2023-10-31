2ba73247fcb982f6c41ea4f87b27caa12fec14e8
Evandro Santos Afonso <evandrosantosafonso@live.com>
2023-10-31 09:55:06 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+function multiplicar(num1, num2) {
+    return num1 * num2;
+}
+function saudacoes(nome) {
+    return `Olá ${nome}`;
+}
---
+++ main.ts
@@ -0,0 +1,7 @@
+function multiplicar(num1: number, num2: number): number{
+    return num1 * num2
+}
+
+function saudacoes (nome: string): string{
+    return `Olá ${nome}`
+}
